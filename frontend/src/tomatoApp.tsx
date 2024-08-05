9364d1b05db1cf761c02a16810584d8c06bd596e
reesedog <hghghghg110@126.com>
2024-08-05 05:51:02 +0000

    feat: status and color transition

--- frontend/src/tomatoApp.tsx
+++ frontend/src/tomatoApp.tsx
@@ -6,51 +6,71 @@ import 'react-circular-progressbar/dist/styles.css';
 
 function TomatoApp() {
   const WORK_TIME = 25;
-  const BREAK_TIME = 25;
-  const [timeLeft, setTimeLeft] = useState(WORK_TIME*60);
-  const [isRunning, setIsRunning] = useState(false);
+  const BREAK_TIME = 5;
+  enum EnumStatus {
+    Working,
+    Breaking,
+    Stopped,
+  }
+  const [timeLeft, setTimeLeft] = useState(WORK_TIME * 60);
+  const [status, setStatus] = useState(EnumStatus.Stopped);
+  const [timerColor, setTimerColor] = useState(`#3b9ed4`);
 
   useEffect(() => {
     let timer: NodeJS.Timeout;
-    if (isRunning) {
+    if ((status == EnumStatus.Working) || (status == EnumStatus.Breaking)) {
       timer = setInterval(() => {
         setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
       }, 1000);
     }
 
     return () => clearInterval(timer);
-  }, [isRunning]);
+  }, [status]);
 
   useEffect(() => {
     if (timeLeft === 0) {
-      setIsRunning(false);
-      setTimeLeft(WORK_TIME*60);
+      if (status === EnumStatus.Working) {
+        setTimeLeft(BREAK_TIME * 60);
+        setStatus(EnumStatus.Breaking);
+        setTimerColor(`#68cc8b`);
+      }
+      if (status === EnumStatus.Breaking) {
+        setTimeLeft(WORK_TIME * 60);
+        setStatus(EnumStatus.Working);
+        setTimerColor(`#3b9ed4`);
+      }
     }
   }, [timeLeft]);
 
   const handleStartPause = () => {
-    setIsRunning(!isRunning);
-    setTimeLeft(BREAK_TIME*60);
-  }
+    if (status === EnumStatus.Stopped) {
+      setStatus(EnumStatus.Working);
+      setTimeLeft(WORK_TIME * 60);
+    } else {
+      setStatus(EnumStatus.Stopped);
+      setTimerColor(`#3b9ed4`);
+      setTimeLeft(WORK_TIME * 60);
+    }
 
-  function tick() {
-    if (isRunning) setTimeLeft(timeLeft => timeLeft - 1);
-    if (timeLeft === 0) setIsRunning(false);
   }
-  
-  
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="w-200 h-200">
-      <CircularProgressbar 
-          styles = {buildStyles({strokeLinecap: 'butt'})}
-          value={timeLeft} 
-          maxValue={WORK_TIME * 60} 
-          text={`${String(Math.floor(timeLeft / 60)).padStart(2, '0')}:${String(timeLeft % 60).padStart(2, '0')}`} 
+        <CircularProgressbar
+          styles={buildStyles({
+            strokeLinecap: 'butt',
+            pathColor: timerColor,
+            textColor: timerColor,
+            trailColor: `white`,
+
+          })}
+          value={timeLeft}
+          maxValue={WORK_TIME * 60}
+          text={`${String(Math.floor(timeLeft / 60)).padStart(2, '0')}:${String(timeLeft % 60).padStart(2, '0')}`}
         />
-      </div> 
-      <button onClick={handleStartPause} className="btn btn-ghost m-4">{isRunning ? 'STOP' : 'START'}</button>
+      </div>
+      <button onClick={handleStartPause} className="btn btn-ghost m-4">{status == EnumStatus.Stopped ? 'START' : 'STOP'}</button>
     </div>
   );
 }
